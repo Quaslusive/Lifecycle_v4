0596e866cf8e3692387c00936b9286c02fd75d2f
Pawlo Picasso <Quaslusive@protonmail.com>
2024-09-03 00:53:27 +0200

    v0.1

---
+++ app/src/main/java/Mobilt_java23/carl_sundberg/lifecyclev4/LoginActivity.kt
@@ -0,0 +1,40 @@
+package Mobilt_java23.carl_sundberg.lifecyclev4
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+        loginButton.setOnClickListener {
+            val username = findViewById<EditText>(R.id.etUsername).text.toString()
+            val password = findViewById<EditText>(R.id.etPassword).text.toString()
+
+            if (username == "user" && password == "password") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+    }
+}
--- app/src/main/java/Mobilt_java23/carl_sundberg/lifecyclev4/MainActivity.java
+++ app/src/main/java/Mobilt_java23/carl_sundberg/lifecyclev4/MainActivity.java
@@ -14,17 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et;
-    Button btn;
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,11 +28,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-et = findViewById(R.id.textView);
-btn = findViewById(R.id.button);
 
-        btn.OnClickListener(e) (->{
-            Intent i = new Intent(Intent.makeMainActivity(this.))
-        });
     }
 }
